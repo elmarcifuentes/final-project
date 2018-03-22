74de4abdcf53b046b6e772760e0d5b0df460bf6f
elmarcifuentes <elmarc@outlook.com>
2018-03-22 12:14:29 -0700

    profile index.js

--- client/src/pages/Dashboard/Profile/index.js
+++ client/src/pages/Dashboard/Profile/index.js
@@ -1,16 +1,1 @@
-import React from 'react';
-import UserAvatar from './UserAvatar';
-import UpdateLogin from './UpdateLogin';
-import UpdateContact from './UpdateContact';
-
-const Profile = () => (
-    <div className="container">
-        <UserAvatar />
-        <hr/>
-        <UpdateLogin />
-        <hr/>
-        <UpdateContact />
-    </div>
-)
-
-export default Profile;
+export { default } from "./Profile";
