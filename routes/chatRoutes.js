65c19610e1a369dbca0f6b1407a95f99f1c04628
rdo <ryan@libertylearner.com>
2018-03-21 23:24:33 -0700

    building POST /newConversation/:recipient

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,16 +1,20 @@
 const path = require("path");
 const router = require("express").Router();
-const db = require('../models/')
+const db = require('../models/');
 
 
 
-
+const authRoutes = require('./authRoutes');
 
 
 // With the schemas created, we can start creating our API endpoints,
 // which will allow us to
 // SEND, DELTE, EDIT AND VIEW messages
 
+
+
+//0. Be able to create a conversation between two users, Alice and Bob.
+//
 
 //1. Be able to get conversationsId for all conversations a single user has.
 //'Knowing' who that single user is is something that I got to work with Jason with
@@ -23,9 +27,11 @@ const db = require('../models/')
 
 
 //This needs a lot of work because it has to utilize an authetnticated user
-router.get('/getConversations', function(req,res){
-    console.log('/getConversations called')
-//console.log(req.user)
+router.get('/:userId', function(req,res){
+    console.log('/chat/userId OR getConversations called, retrieving list of conversations given userId')
+    
+    
+    console.log(req.params.userId)
     //somehow, this call needs to be able to get the user's id. However the authentication works, this must work with it.
     var id = req.query.id;
     
@@ -76,11 +82,38 @@ router.get('/:conversationId', function(req,res){
 
 
 
-router.post('/newConversation', function(req,res){
-    console.log('post /newConversation')
-    console.log(req)
 
 
+//Make a conversation, a function that utilizes the message objects to build
+//conversations
+router.post('/newConversation/:recipient', function(request,response){
+    console.log('post /newConversation/:recipient called OR createConversation')
+    console.log(request.params)
+    
+    //logic for making sure a valid recipient is in params FIRST
+
+    if(!request.params.recipient){
+      response.status(442).send({error: 'WE NEED valid recipient(userId)'})
+      //not sure about the next but this was the return used in the example.
+      return next;
+    }
+
+
+    //same idea but for the body message. This serves as the origin for recipient
+    //if(!req.body.messageBody){}
+
+
+
+    db.UserConversation.create()
+
+    if(true){
+
+
+      response.json({
+
+        message:'ITS WORKING'
+      })
+    }
 
 })
 
