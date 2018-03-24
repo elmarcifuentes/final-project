a9fdd8f5dc173cb6823fdcc5d218f0a2a6026aa5
elmarcifuentes <elmarc@outlook.com>
2018-03-23 19:55:36 -0700

    linked registration page to authentication

--- client/src/pages/Home/RegisterForm.js
+++ client/src/pages/Home/RegisterForm.js
@@ -1,21 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from "axios";
 
-const RegisterForm = () => (
+class RegisterForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: "",
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            accountStatus: ""
+        }
+        this.submit = this.submit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+    submit(e) {
+        e.preventDefault();
+        axios.post("/auth/register", this.state)
+        .then(resp => {
+            console.log(resp);
+            this.props.history.push("/login");
+        })
+        .catch(err => {
+            console.error(err);
+        })
+    }
+    render() {
+        return (
 <div className="card">
     <div className="card-body">
         <form>
             <div className="form-group">
+                <label for="exampleInputFirstName">First Name</label>
+                <input type="firstName" name="firstName" onChange={this.handleInputChange} className="form-control form-control-lg" id="exampleInputFirstName" aria-describedby="FirstNameHelp" placeholder="First Name" />
+            </div>
+            <div className="form-group">
+                <label for="exampleInputLastName">Last Name</label>
+                <input type="lastName" name="lastName" onChange={this.handleInputChange} className="form-control form-control-lg" id="exampleInputLastName" aria-describedby="LastNameHelp" placeholder="Last Name" />
+            </div>
+            <div className="form-group">
                 <label for="exampleInputUsername">Username</label>
-                <input type="text" className="form-control form-control-lg" id="exampleInputUsername" aria-describedby="usernameHelp" placeholder="Pick a username" />
+                <input type="username" name="username" onChange={this.handleInputChange} className="form-control form-control-lg" id="exampleInputUsername" aria-describedby="usernameHelp" placeholder="Pick a Username" />
+            </div>
+            <div className="form-group">
+                <label for="exampleInputUsername">Status</label>
+                <input type="accountStatus" name="accountStatus" onChange={this.handleInputChange} className="form-control form-control-lg" id="exampleInputStatus" aria-describedby="StatusHelp" placeholder="Tenant or Manager" />
             </div>
             <div className="form-group">
                 <label for="exampleInputEmail">Email address</label>
-                <input type="email" className="form-control form-control-lg" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter email" />
+                <input type="email" name="email" onChange={this.handleInputChange} className="form-control form-control-lg" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Email" />
                 <small id="emailHelp" className="form-text">We'll never share your email with anyone else.</small>
             </div>
             <div className="form-group">
                 <label for="exampleInputPassword">Password</label>
-                <input type="password" className="form-control form-control-lg" id="exampleInputPassword" placeholder="Password" />
+                <input type="password" name="password" onChange={this.handleInputChange} className="form-control form-control-lg" id="exampleInputPassword" placeholder="Password" />
             </div>
             <div className="mb-3">
                 <div className="custom-control custom-checkbox">
@@ -24,10 +72,12 @@ const RegisterForm = () => (
                     </label>
                 </div>
             </div>
-            <button type="submit" className="btn btn-lg btn-danger btn-block">NewsFlash Sign Up</button>
+            <button type="submit" className="btn btn-lg btn-danger btn-block" onClick={this.submit}>NewsFlash Sign Up</button>
         </form>
     </div>
 </div>
-)
+        );
+    }
+}
 
 export default RegisterForm;
--- client/src/pages/Register/RegisterForm.js
+++ client/src/pages/Register/RegisterForm.js
@@ -1,37 +1,10 @@
 import React from 'react';
+import HomeRegister from '../Home/RegisterForm';
 
 const RegisterForm = () => (
     <div className="col-12 col-lg-8 align-self-center">
-                      <div className="text-center mb-5">
-                          <h1 className="h2 mb-2">Register</h1>
-                          <span>Register now and be part of NewsFlash!</span>
-                      </div>
-                      <form className="mb-4">
-                          <div className="form-group">
-                              <label for="signup-email">Email Address</label>
-                              <input className="form-control form-control-lg" type="email" name="email" id="signup-email" placeholder="Email Address" />
-                          </div>
-                          <div className="form-group">
-                              <label for="signup-password">Choose a password</label>
-                              <input className="form-control form-control-lg" type="password" name="password" id="signup-password" placeholder="Enter a password" />
-                              <small>At least seven characters with one numeral</small>
-                          </div>
-                          <div>
-                              <div className="custom-control custom-checkbox">
-                                  <input type="checkbox" className="custom-control-input" value="agree" name="agree-terms" id="check-agree"/>
-                                  <label className="custom-control-label text-small" for="check-agree">I agree to the <a href="/">Terms &amp; Conditions</a>
-                                  </label>
-                              </div>
-                          </div>
-                          <div className="text-center mt-4">
-                              <button type="submit" className="btn btn-lg btn-danger">Create Account</button>
-                          </div>
-                      </form>
-                      <div className="text-center">
-                          <span className="text-small">Already have an account? <a href="/signin">Log in here</a>
-                          </span>
-                      </div>
-                  </div>
+        <HomeRegister />
+    </div>
 )
 
 export default RegisterForm;
